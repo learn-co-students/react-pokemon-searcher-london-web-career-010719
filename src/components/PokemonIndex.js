2596c2341d730b76bd43929f62abfd3df352ebe5
YannickHoegerle <yannick.hoegerle@gmail.com>
2019-03-21 11:24:47 +0000

    Done.

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -4,17 +4,62 @@ import PokemonForm from './PokemonForm'
 import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
+const API = "http://localhost:3000/pokemon"
+
 class PokemonPage extends React.Component {
+
+  state = {
+    allPokemons: [],
+    searchTerm: ''
+  }
+
+  componentDidMount () {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(pokemons => this.setState({allPokemons: pokemons}))
+  }
+
+  addNewPokemon = (data) => {
+    const newPokemon = {
+      "name": data.name,
+      "stats": [
+        {
+          "value": data.hp,
+          "name": "hp"
+        }
+      ],
+      "sprites": {
+        "front": data.frontUrl,
+        "back": data.backUrl
+      }
+    }
+    return fetch(API, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newPokemon)
+    })
+      .then(res => res.json())
+      .then(pokemons => this.setState({ allPokemons: pokemons }))
+  }
+
+  handleSearchChange = (event) => {
+    this.setState({searchTerm: event.target.value})
+  }
+
+  displayPokemons = () => this.state.allPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={this.handleSearchChange} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection allPokemons={this.displayPokemons()} />
         <br />
-        <PokemonForm />
+        <PokemonForm allPokemons={this.displayPokemons()} addNewPokemon={this.addNewPokemon}/>
       </div>
     )
   }
